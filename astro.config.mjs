9a1048ea373eef2621aaecc33871a7f222037f5e
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-10-19 14:38:54 +0800

    chore: Deploy to deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import vue from "@astrojs/vue"
 // @ts-check
+import vue from "@astrojs/vue"
+import deno from "@deno/astro-adapter"
 import { defineConfig } from "astro/config"
 import UnoCSS from "unocss/astro"
 
 export default defineConfig({
+  output: "server",
+  adapter: deno(),
   integrations: [UnoCSS({
     injectReset: true,
   }), vue()],
