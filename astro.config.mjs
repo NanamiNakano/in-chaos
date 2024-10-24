f48f0628cb537df876c58001acd834fe0a715dc1
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-10-24 18:36:22 +0800

    ci(deploy): Switch to static deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import vue from "@astrojs/vue"
-import deno from "@deno/astro-adapter"
 import { defineConfig } from "astro/config"
 import UnoCSS from "unocss/astro"
 
 export default defineConfig({
-  output: "server",
-  adapter: deno(),
   integrations: [UnoCSS({
     injectReset: true,
   }), vue()],
